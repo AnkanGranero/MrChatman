6e1646b569feabba61320a6118bc3fbd3f0991e7
Andreas <grannemannen@gmail.com>
2019-03-13 09:00:58 +0100

    ändrad firebasekod

--- firebase.js
+++ firebase.js
@@ -1,18 +1,24 @@
 import firebase from "firebase/app";
 
+
+
+
 var config = {
-    apiKey: "AIzaSyDh8sWDa8mAXM58tAuDlWptTaWUmGkvFwc",
-    authDomain: "andreasproject-886d1.firebaseapp.com",
-    databaseURL: "https://andreasproject-886d1.firebaseio.com",
-    projectId: "andreasproject-886d1",
-    storageBucket: "andreasproject-886d1.appspot.com",
-    messagingSenderId: "987426855419"
-    // apiKey: "AIzaSyDoB1u4OHtsT9te8pN0dTk0cM68SFS6NQk",
-    // authDomain: "customers-c4cf2.firebaseapp.com",
-    // databaseURL: "https://customers-c4cf2.firebaseio.com",
-    // projectId: "customers-c4cf2",
-    // storageBucket: "",
-    // messagingSenderId: "793398235205"
+    apiKey: "AIzaSyArU8cNt1fjZykEGN3Zs4pWxs11so7sA2I",
+    authDomain: "mrchatman-51b04.firebaseapp.com",
+    databaseURL: "https://mrchatman-51b04.firebaseio.com",
+    projectId: "mrchatman-51b04",
+    storageBucket: "mrchatman-51b04.appspot.com",
+    messagingSenderId: "648933994123"
 };
+
+
+// apiKey: "AIzaSyDoB1u4OHtsT9te8pN0dTk0cM68SFS6NQk",
+// authDomain: "customers-c4cf2.firebaseapp.com",
+// databaseURL: "https://customers-c4cf2.firebaseio.com",
+// projectId: "customers-c4cf2",
+// storageBucket: "",
+// messagingSenderId: "793398235205"
+
 firebase.initializeApp(config);
 export default firebase; 
